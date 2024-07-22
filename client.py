1aa8a34839911326bc97f56c12b5781822c61bbf
NCC79601 <ncc79601@163.com>
2024-07-22 14:27:12 +0800

    refactor: rewrite sever and client as classes

--- client.py
+++ client.py
@@ -1,25 +1,45 @@
-# 客户端
+# for client
 import cv2
 import socket
 import json
 
-with open('config.json', "r") as f:
-    config = json.load(f)
+class Client(object):
+    def __init__(self, server_ip: str, server_port: int = 8888):
+        '''
+        Initialize a Client object.
 
-server_ip   = config["server_ip"]
-server_port = config["server_port"]
+        Parameters:
+        - server_ip: ip of the server
+        - server_port: port of the server, default: 8888
+        '''
+        self.server_ip = server_ip
+        self.server_port = server_port
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-capture = cv2.VideoCapture(0)
-while True:
-    # 采集视频帧
-    ret, frame = capture.read()
-    if not ret:
-        break
-    # 对视频帧进行编码压缩
-    _, data = cv2.imencode('.jpg', frame)
-    # 发送压缩后的视频数据
-    client.sendto(data.tobytes(), (server_ip, server_port))
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.capture = cv2.VideoCapture(0)
 
-capture.release()
-cv2.destroyAllWindows()
+    def run(self):
+        '''Run the client.'''
+        while True:
+            # Capture video frame
+            ret, frame = self.capture.read()
+            if not ret:
+                break
+            # Resize the video frame to 480p
+            frame = cv2.resize(frame, (160, 120))
+            # Encode and compress the video frame
+            _, data = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+            print(f'Frame shape: {frame.shape}')
+            print(f'Data size: {len(data.tobytes())}')
+            # Send the compressed video data
+            self.client.sendto(data.tobytes(), (self.server_ip, self.server_port))
+
+        self.capture.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    with open('config.json', "r") as f:
+        config = json.load(f)
+    client = Client(server_ip=config['server_ip'], server_port=config['server_port'])
+    client.run()
--- server.py
+++ server.py
@@ -1,25 +1,41 @@
-# 服务端
+# for servers
 import cv2
 import socket
 import numpy as np
 import json
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
+class Server(object):
+    def __init__(self, server_ip: str = '127.0.0.1', server_port: int = 8888):
+        '''
+        Initialize a Server object.
 
-server_ip   = config['server_ip']
-server_port = config['server_port']
+        Parameters:
+        - server_ip: ip of the server, default: `127.0.0.1`
+        - server_port: port of the server, default: 8888
+        '''
+        self.server_ip = server_ip
+        self.server_port = server_port
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('127.0.0.1', server_port))
-while True:
-    # 接收客户端发送的数据
-    data, addr = server.recvfromx(65535) # 一次最多接收 65535 个字节的数据
-    # 解码压缩后的视频数据
-    frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
-    # 显示解码后的视频数据
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-server.close()
-cv2.destroyAllWindows()
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server.bind((self.server_ip, self.server_port))
+
+    def run(self):
+        '''Run the server.'''
+        while True:
+            # Receive data from the client
+            data, addr = self.server.recvfrom(65535) # 65535 bytes of data maximum
+            frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+        
+        self.server.close()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    server = Server(server_ip='127.0.0.1', server_port=config['server_port'])
+    server.run()
