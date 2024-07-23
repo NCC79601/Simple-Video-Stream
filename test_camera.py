a0a7414e9eba2d4f772dbc041e2a1b665ca96155
NCC79601 <ncc79601@163.com>
2024-07-23 15:27:46 +0800

    feat: add test_camera.py

---
+++ test_camera.py
@@ -0,0 +1,14 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(1)
+time.sleep(1)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print('Camera offline. Exiting...')
+        break
+    cv2.imshow('Video Capture', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
